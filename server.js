b5d75e6108cc0154e70f6da681a34f44ebe23487
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-09-24 05:50:20 +0700

    Added Mongoose, EJS engine, and reinit package-lock

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
