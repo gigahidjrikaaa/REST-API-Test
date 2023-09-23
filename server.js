b2662b179977e75a135e0c139553e2049139e8ba
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-09-24 05:42:58 +0700

    Initialized mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
