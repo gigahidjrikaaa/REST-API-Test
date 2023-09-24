10a084ae101f0ca35e83f42681e5d0a3852c1837
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-09-25 00:53:44 +0700

    Added mongoDB driver & logging

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const port = 3000;
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+console.log(process.env.DATABASE_URL);
 const db = mongoose.connection;
-db.on('error', error => console.error(error));
+db.on('error', error => console.error("MongoDB Connection error: ", error));
 db.once('open', () => console.log('Connected to Database'));
 
 // Set EJS as templating engine
